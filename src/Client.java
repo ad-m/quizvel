305d04e297c9b1d05889cf422f98acbb88923d5b
Adam Dobrawy <naczelnik@jawnosc.tk>
2016-06-10 10:18:30 +0200

    Server health check on startup

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,9 @@
 
 import java.awt.EventQueue;
 
+import javax.swing.JOptionPane;
+
+import client.dao.DAO;
 import client.window.LoginWindow;
 
 public class Client {
@@ -8,7 +11,12 @@ public class Client {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					new LoginWindow();
+					if (DAO.getInstance().healt_check()) {
+						new LoginWindow();
+					} else {
+						JOptionPane.showMessageDialog(null, "No connection to server.", "Connection error",
+								JOptionPane.ERROR_MESSAGE);
+					}
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
